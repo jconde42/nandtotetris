00dfd5e59ce603cc03a297be4d72a41ac882c2d6
Janner Conde <condejanner@gmail.com>
2016-06-03 13:41:30 -0600

    Mult.asm done

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,48 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+(Start)
+        @R0
+        D=M
+        @x
+        M=D // x = RAM[0]
+
+
+        @R1
+        D=M
+        @y
+        M=D // y = RAM[1]
+
+
+        @i
+        M=0 // i = 0
+
+        @R2
+        M=0 // init RAM[2]
+
+(LOOP)
+        @y
+        D=M
+        @i
+        D=D-M
+
+        @END
+        D;JEQ   // if i == y goto END
+
+        @i
+        M=M+1 // inc i
+
+        @x
+        D=M
+
+        @R2
+        M=M+D // RAM[2] = RAM[2] + RAM[0]
+
+        @LOOP
+        0;JMP
+
+(END)
+        @END
+        0;JMP
